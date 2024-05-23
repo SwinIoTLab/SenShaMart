95b626629e42398a2890af2dd1dcb4bd96ca806b
Anas-Dawod <43335798+Anas-Dawod@users.noreply.github.com>
2024-05-23 15:41:16 +1000

    Update commit.ts

--- blockchain/commit.ts
+++ blockchain/commit.ts
@@ -13,10 +13,7 @@
  *
  *    You should have received a copy of the GNU Lesser General Public License
  *    along with SenShaMart.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * @author Josip Milovac
- */
+ **/
 
 /**
  * @author Anas Dawod e-mail: adawod@swin.edu.au
