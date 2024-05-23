9296dd88969af785439f5a89066f5fe34e1c60bb
Anas-Dawod <43335798+Anas-Dawod@users.noreply.github.com>
2024-05-23 15:35:15 +1000

    Update miner.ts

--- miner/miner.ts
+++ miner/miner.ts
@@ -1,3 +1,24 @@
+/**
+ *    Copyright (c) 2022-2024, SenShaMart
+ *
+ *    This file is part of SenShaMart.
+ *
+ *    SenShaMart is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License.
+ *
+ *    OpenIoT is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with OpenIoT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * @author Anas Dawod e-mail: adawod@swin.edu.au
+ */
 import Block from '../blockchain/block.js';
 
 import { isFailure, type Result } from '../util/chain-util.js';
