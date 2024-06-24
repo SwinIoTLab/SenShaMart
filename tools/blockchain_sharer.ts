05824756b7cb71e6e2b10eb4eb8b1117a9762747
Josip Milovac <paci.paci@yahoo.com.au>
2024-06-24 12:33:08 +1000

    update to blockchain sharer, make existing files reference counted to stop potential corruption if reading the share db happens during an update of the share db

--- tools/blockchain_sharer.ts
+++ tools/blockchain_sharer.ts
@@ -1,7 +1,7 @@
 import { Blockchain, Persistence } from '../blockchain/blockchain.js';
 import { PropServer, type SocketConstructor } from '../network/blockchain-prop.js';
 import { WebSocket, WebSocketServer } from 'ws';
-import { promises as fs } from 'fs';
+import { promises as fsPromises, createReadStream } from 'fs';
 import { default as express } from 'express';
 
 const UPDATE_TIME = 60 * 60 * 1000; //1 hour
@@ -11,7 +11,7 @@ if (process.argv.length < 2) {
   process.exit(-1);
 }
 if (process.argv.length < 7) {
-  console.error(`Expected 'node "${process.argv[1]}" <active blockchain location> <sharing blockchain location> <chain server port> <peers> <sharing port>`);
+  console.error(`Expected 'node "${process.argv[1]}" <active blockchain location> <sharing blockchain stem> <chain server port> <peers> <sharing port>`);
   process.exit(-2);
 }
 
@@ -23,8 +23,21 @@ const sharingPort = Number.parseInt(process.argv[6]);
 
 const blockchain = await Blockchain.create(activeBlockchainLocation, null);
 
+type SharingFile = {
+  location: string;
+  usingCount: number;
+};
+
 const chainServer = new PropServer("Chain-server", blockchain, WebSocket as unknown as SocketConstructor, WebSocketServer);
 chainServer.start(chainServerPort, '', JSON.parse(peers));
+
+let sharingFileOn = 0;
+//we start on usingCount 2, so our first copy Blockchain doesn't try and remove the file which doesn't exist.
+//This never goes to 0 (since no one is actually using it) and so the non-existant file is never deleted
+let currentSharingFile: SharingFile = {
+  location: null,
+  usingCount: 2
+};
 
 async function copyBlockchain() {
   console.log("Creating sharing blockchain");
@@ -44,7 +57,19 @@ async function copyBlockchain() {
       resolve();
     }
   }));
-  await fs.copyFile(activeBlockchainLocation, passiveBlockchainLocation);
+  const prevSharing = currentSharingFile;
+  ++sharingFileOn;
+  const newSharing = {
+    location: `${passiveBlockchainLocation}.${sharingFileOn}.db`,
+    usingCount: 1 //start with us using it
+  };
+  await fsPromises.copyFile(activeBlockchainLocation, newSharing.location);
+  currentSharingFile = newSharing;
+  --prevSharing.usingCount; //we aren't using the prev any more
+  if (prevSharing.usingCount === 0) {
+    //we don't wait on this, because we don't care about if it fails, and it doesn't impact anything as it's just cleanup of unused files
+    fsPromises.rm(prevSharing.location);
+  }
   blockchain.persistence = await Persistence.openDb(activeBlockchainLocation);
   setTimeout(copyBlockchain, UPDATE_TIME);
 }
@@ -60,6 +85,18 @@ const add_static_file = (url: string, location: string, type: string) => {
     });
   });
 };
+
+app.get('/blockchain.db', (_req, res) => {
+  res.setHeader('content-type', 'octet-stream');
+  const sharingFile = currentSharingFile;
+  ++sharingFile.usingCount;
+  createReadStream(sharingFile.location).pipe(res).on("finish", () => {
+    --sharingFile.usingCount;
+    if (sharingFile.usingCount === 0) {
+      fsPromises.rm(sharingFile.location);
+    }
+  });
+});
 
 add_static_file('/blockchain.db', passiveBlockchainLocation, '.db');
 
